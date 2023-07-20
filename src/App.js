111f27a507a6bef0c9e4509f958cd34e7c7ca84b
RumeysaaKopuz <rumeysakpzz@gmail.com>
2023-07-20 23:31:07 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Sayac from './components/ClassComp';
+import Paper from './components/FunctionComponent';
+import Search from './components/refTest';
+import FilterUser from './components/FilterUser';
 
 function App() {
+  const [showSayac, setShowSayac] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <button onClick={() => setShowSayac(!showSayac)}>
+        {showSayac ? 'Gizle' : 'Göster'}
+      </button>
+
+      {showSayac && <Paper />} 
+      
+      <Search />
+      */}
+
+      <FilterUser />
     </div>
   );
 }
